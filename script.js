3c0a5a377071d07a71d9bd238d688f2d19ced234
Akshat Singh <akshatofficial041@gmail.com>
2024-10-26 16:28:21 +0530

    added grid size change and reset button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const container = document.querySelector(".grid-container");
+const popup = document.querySelector(".popup-btn");
+const reset = document.querySelector(".reset");
 
 const gridMaker = function (size = 16) {
   for (let i = 0; i < size; i++) {
@@ -24,7 +26,26 @@ const gridMaker = function (size = 16) {
 
   items.forEach((item) => {
     item.setAttribute("style", `border: 1px solid black`);
+    item.addEventListener("mouseover", () => {
+      item.setAttribute("style", "background-color: black");
+    });
   });
 };
+
+popup.addEventListener("click", () => {
+  const grid_size = Number(prompt("Enter new grid size"));
+  if (grid_size <= 100) {
+    container.innerHTML = "";
+    gridMaker(grid_size);
+  } else alert("Please enter size between less than 100");
+});
+
+reset.addEventListener("click", () => {
+  const items = document.querySelectorAll(".grid-item");
+
+  items.forEach((item) => {
+    item.setAttribute("style", `background: null; border: 1px solid black`);
+  });
+});
 
 gridMaker();
