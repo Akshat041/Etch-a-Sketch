af78343d3bf3ef2f09e1cf8db9667b2deabc4b7a
Akshat Singh <akshatofficial041@gmail.com>
2024-10-27 15:54:26 +0530

    Made some changes in styling and added buttons for Shade mode and color mode

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const container = document.querySelector(".grid-container");
 const popup = document.querySelector(".popup-btn");
 const reset = document.querySelector(".reset");
+const shade = document.querySelector(".black");
+const coloured = document.querySelector(".color");
 
 const gridMaker = function (size = 16) {
   for (let i = 0; i < size; i++) {
@@ -17,20 +19,31 @@ const gridMaker = function (size = 16) {
     "style",
     `display: grid; grid-template-columns: repeat(${size}, ${
       800 / size
-    }px); grid-template-rows: repeat(${size}, ${
-      800 / size
-    }px); width: 800px; border: 1px solid black; box-sizing: content-box; margin: 20px auto`
+    }px); grid-template-rows: repeat(${size}, ${800 / size}px)`
   );
 
   const items = document.querySelectorAll(".grid-item");
 
   items.forEach((item) => {
     item.setAttribute("style", `border: 1px solid black`);
-    const r = Math.trunc(Math.random() * 255 + 1);
-    const g = Math.trunc(Math.random() * 255 + 1);
-    const b = Math.trunc(Math.random() * 255 + 1);
-    let o = 0.1;
 
+    let r = 0,
+      g = 0,
+      b = 0;
+
+    shade.addEventListener("click", () => {
+      r = 0;
+      g = 0;
+      b = 0;
+    });
+
+    coloured.addEventListener("click", () => {
+      r = Math.trunc(Math.random() * 255 + 1);
+      g = Math.trunc(Math.random() * 255 + 1);
+      b = Math.trunc(Math.random() * 255 + 1);
+    });
+
+    let o = 0.1;
     item.addEventListener("mouseover", () => {
       o = o + 0.1;
       item.setAttribute(
